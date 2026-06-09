6eb55b84af81ee2a8c1d0b9e55ce8a91b9129694
jesanta <jesanta26@gmail.com>
2026-06-08 18:37:36 -0600

    Version 1

--- app.py
+++ app.py
@@ -53,6 +53,6 @@ if start_button:
     mean = toss_coin(number_of_trials)
     st.session_state['df_experiment_results'] = pd.concat([st.session_state['df_experiment_results'],pd.DataFrame(data=[[st.session_state['experiment_no'],number_of_trials,mean]],columns=['no', 'iteraciones', 'media'])],axis=0)
     
-	st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
+    st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
 
 st.write(st.session_state['df_experiment_results'])
